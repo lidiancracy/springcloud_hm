e951182d4320ed1de1b989fa7487c851c8b5b492
lidian <837997288@qq.com>
2022-10-05 19:36:17 +0800

    nacos配置文件

--- user/src/main/java/com/example/user/service/UserService.java
+++ user/src/main/java/com/example/user/service/UserService.java
@@ -4,6 +4,7 @@ package com.example.user.service;
 import com.example.user.mapper.UserMapper;
 import com.example.user.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +13,13 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Value("${lidain}")
+    String ld;
+    @Value("${a.b}")
+    String ld2;
     public User queryById(Long id) {
-
+        System.out.println(ld);
+        System.out.println(ld2);
         System.out.println("调用了我的user服务");
         return userMapper.findById(id);
     }
